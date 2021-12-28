bcec3ce6c2ca135602cfa41bc843ab53d8c3e466
Daniele Guglietti <gugliettidaniele@gmail.com>
2021-12-28 16:51:43 +0100

    Added properties and accessors in Transaction.py

--- Transaction.py
+++ Transaction.py
@@ -1,12 +1,38 @@
 """Defines Transaction class
-TODO: properties
 TODO: compute_hash
 TODO: sign
 TODO: check
 """
+from dataclasses import dataclass
 
 
+@dataclass
 class Transaction(object):
+    __sender: str
+    __recipient: str
+    __amount: float
+
+    @property
+    def sender(self) -> str:
+        """Returns the sender of the transaction.
+        :return: The sender of the transaction.
+        """
+        return self.__sender
+
+    @property
+    def recipient(self) -> str:
+        """Returns the recipient of the transaction.
+        :return: The recipient of the transaction.
+        """
+        return self.__recipient
+
+    @property
+    def amount(self) -> float:
+        """Returns the amount of the transaction.
+        :return: The amount of the transaction.
+        """
+        return self.__amount
+
     def compute_hash(self) -> str:
         """Calculates the hash of the transaction.
         :return: The calculated hash.
