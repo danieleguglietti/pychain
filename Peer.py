d8bcbeff5e8d8eaa46e7aa9439c1d26aa2c55e38
Daniele Guglietti <gugliettidaniele@gmail.com>
2021-12-29 16:31:44 +0100

    Added properties in Peer

--- Peer.py
+++ Peer.py
@@ -1,9 +1,20 @@
+from dataclasses import dataclass
 from typing import List
 
 from Block import Block
 
 
+@dataclass
 class Peer(object):
+    __address: str
+
+    @property
+    def address(self) -> str:
+        """Get address of the peer
+        :return: Address
+        """
+        return self.__address
+
     def get_blocks(self) -> List[Block]:
         """Get blocks of the peer
         :return: List of blocks
